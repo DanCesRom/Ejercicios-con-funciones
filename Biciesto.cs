f4b7c9ac3bf181eb2cd337b206a20010f8cd24b3
DanCesRom <123031318+DanCesRom@users.noreply.github.com>
2023-01-18 19:00:00 -0400

    Add files via upload

---
+++ Biciesto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args)
+   {
+    Console.Clear();
+    Console.WriteLine ("Que año quieres revisar si es Biciesto?: ");
+    Console.WriteLine ("\n");
+    decimal x = Convert.ToDecimal(Console.ReadLine());
+    Console.Clear();
+
+    if((x/100)%1 == 0)
+    {
+      if ((x/400)%1 ==0)
+      {
+        Console.WriteLine(x + " Es un año Biciesto");
+      }
+      else
+      {
+        Console.WriteLine(x + " No es un año Biciesto");
+      }
+    }
+    else
+    {
+      if ((x/4)%1 == 0)
+      {
+        Console.WriteLine(x + " Es un año Biciesto");
+      }
+      else
+      {
+        Console.WriteLine(x + " No es un año Biciesto");
+      }
+    }
+  }
+}
